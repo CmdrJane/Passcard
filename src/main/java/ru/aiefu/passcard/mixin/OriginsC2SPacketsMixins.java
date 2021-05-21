249494d377785d6a2fba0596f48cd487fd2c9a3b
CmdrJane <48736985+CmdrJane@users.noreply.github.com>
2021-05-21 16:27:25 +0300

    fixes

--- src/main/java/ru/aiefu/passcard/mixin/OriginsC2SPacketsMixins.java
+++ src/main/java/ru/aiefu/passcard/mixin/OriginsC2SPacketsMixins.java
@@ -18,7 +18,7 @@ import ru.aiefu.passcard.IPlayerPass;
 public class OriginsC2SPacketsMixins {
     @Inject(method = "useActivePowers", at =@At("HEAD"), remap = false, cancellable = true)
     private static void preventActiveUseIfNotLoggedIn(MinecraftServer minecraftServer, ServerPlayerEntity playerEntity, ServerPlayNetworkHandler serverPlayNetworkHandler, PacketByteBuf packetByteBuf, PacketSender packetSender, CallbackInfo ci){
-        if( !((IPlayerPass)serverPlayNetworkHandler).getAuthState() ){
+        if( !((IPlayerPass)playerEntity).getAuthState() ){
             ci.cancel();
             return;
         }
